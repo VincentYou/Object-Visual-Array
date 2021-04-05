2a7910cb5730d1027e1c3668aa06bb313b25feac
VincentYou <vincent.you23@sitechhs.com>
2021-04-05 08:17:37 -0400

    hmm

--- src/js/index.js
+++ src/js/index.js
@@ -1,94 +1,22 @@
-DOMSelectors = {
-  displayContainer: document.querySelector(".container"),
-  resetButton: document.querySelector(".reset-btn"),
-  fullMenuButton: document.querySelector(".full-btn"),
-  inStockButton: document.querySelector(".stock-btn"),
-  vegetarianButton: document.querySelector(".veg-btn"),
-};
+import {DOMSelectors} from "./DOM";
+import {menu, veganOptions,  instockOptions} from "./menu";
 
-const menu = [
-    {
-      name: "Pizza Slice",
-      vegetarian: true,
-      price: 2.0,
-      img:
-        "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      inStock: true,
-    },
-    {
-      name: "Salad",
-      vegetarian: true,
-      price: 5.0,
-      img:
-        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      inStock: true,
-    },
-    {
-      name: "Soup",
-      vegetarian: true,
-      price: 3.0,
-      img:
-        "https://images.unsplash.com/photo-1547592166-23ac45744acd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
-      inStock: true,
-    },
-    {
-      name: "Grilled Cheese",
-      vegetarian: true,
-      price: 4,
-      img:
-        "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
-      inStock: false,
-    },
-    {
-      name: "Roast Chicken",
-      vegetarian: false,
-      price: 8.0,
-      img:
-        "https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      inStock: true,
-    },
-    {
-      name: "Steak",
-      vegetarian: false,
-      price: 10.0,
-      img:
-        "https://images.unsplash.com/photo-1574969884448-fe5bce3d0d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80",
-      inStock: false,
-    },
-    {
-      name: "Cheeseburger",
-      vegetarian: false,
-      price: 6,
-      img:
-        "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=902&q=80",
-      inStock: true,
-    },
-    {
-      name: "Ice Cream",
-      vegetarian: false,
-      price: 3.0,
-      img:
-        "https://images.unsplash.com/photo-1566454419290-57a64afe30ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      inStock: true,
-    },
-  ];
-
-  const init = function (){
+const init = function (){
     menu.forEach((item) =>//for every item, it will insert HTML that we will use to show
       DOMSelectors.displayContainer.insertAdjacentHTML(
       "afterbegin",
       `<ul class="item-list">
           <li class="item-name item-value">${item.name}</li>
           <li class="item-price item-value">${item.price}</li>
-          <li class="item-vegetarian item-value">${item.vegitarian}</li>
+          <li class="item-vegetarian item-value">${item.vegetarian}</li>
           <li class="item-image">
        <img
          class="item-image"
-         src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+         src="${item.img}"
          alt=""
        />
      </li>
-     <li class="item-in-stock item-value">In Stock</li>
+     <li class="item-in-stock item-value">${item.inStock}</li>
    </ul>`
    ) 
     );
@@ -102,22 +30,18 @@ const menu = [
         `<ul class="item-list">
             <li class="item-name item-value">${item.name}</li>
             <li class="item-price item-value">${item.price}</li>
-            <li class="item-vegetarian item-value">${item.vegitarian}</li>
+            <li class="item-vegetarian item-value">${item.vegetarian}</li>
             <li class="item-image">
          <img
            class="item-image"
-           src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+           src="${item.img}"
            alt=""
          />
        </li>
-       <li class="item-in-stock item-value">In Stock</li>
+       <li class="item-in-stock item-value">${item.inStock}</li>
      </ul>`
      ); 
     })
     
   }); 
   init();
-
-  const veganOptions = menu.filter((item) => item.vegetarian === true); //the arrow function return whats inside the function
-  const instockOptions = menu.filter((item) => item.inStock === true);
-  console.log(veganOptions);
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,7 @@
-DOMSelectors = {
-  displayContainer: document.querySelector(".container"),
-  resetButton: document.querySelector(".reset-btn"),
-  fullMenuButton: document.querySelector(".full-btn"),
-  inStockButton: document.querySelector(".stock-btn"),
-  vegetarianButton: document.querySelector(".veg-btn"),
-};
-
 const menu = [
     {
       name: "Pizza Slice",
-      vegetarian: False,
+      vegetarian: true,
       price: 2.0,
       img:
         "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
@@ -73,50 +65,6 @@ const menu = [
     },
   ];
 
-  const init = function (){
-    menu.forEach((item) =>//for every item, it will insert HTML that we will use to show
-      DOMSelectors.displayContainer.insertAdjacentHTML(
-      "afterbegin",
-      `<ul class="item-list">
-          <li class="item-name item-value">${item.name}</li>
-          <li class="item-price item-value">${item.price}</li>
-          <li class="item-vegetarian item-value">${item.vegitarian}</li>
-          <li class="item-image">
-       <img
-         class="item-image"
-         src="${item.img}"
-         alt=""
-       />
-     </li>
-     <li class="item-in-stock item-value">${item.inStock}</li>
-   </ul>`
-   ) 
-    );
-  };
-
-  DOMSelectors.vegetarianButton.addEventListener("click", function(e){
-    DOMSelectors.displayContainer.innerHTML = "";
-    veganOptions.forEach((item) => {//add the array method for the vegan 
-      DOMSelectors.displayContainer.insertAdjacentHTML(
-        "afterbegin",
-      `<ul class="item-list">
-          <li class="item-name item-value">${item.name}</li>
-          <li class="item-price item-value">${item.price}</li>
-          <li class="item-vegetarian item-value">${item.vegitarian}</li>
-          <li class="item-image">
-       <img
-         class="item-image"
-         src="${item.img}"
-         alt=""
-       />
-     </li>
-     <li class="item-in-stock item-value">${item.inStock}</li>
-   </ul>`
-     ); 
-    })
-  }); 
-  init();
-
   const veganOptions = menu.filter((item) => item.vegetarian === true); //the arrow function return whats inside the function
   const instockOptions = menu.filter((item) => item.inStock === true);
-  console.log(veganOptions);
+  export{veganOptions, instockOptions, menu};
